9d93e95c18456fdb13830dac3f7d20878d2e4290
Stefan Vladetic <stefanvladetic@a-yr246lvcg.carfax.cfx>
2020-11-11 17:27:18 -0500

    installed react types to fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import DropdownComponent from "./app/DropdownComponent/DropdownComponent";
 
@@ -5,23 +6,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <span className="button">LOGO</span>
-        <a className="button" href="https://canadapproval.com/">
+        <span className="header-text">LOGO</span>
+        <a className="header-text" href="https://canadapproval.com/">
           HOME
         </a>
         <DropdownComponent></DropdownComponent>
         <a
-          className="button"
+          className="header-text"
           href="https://canadapproval.com/common-questions-asked/"
         >
           FAQ
         </a>
         <DropdownComponent></DropdownComponent>
         <DropdownComponent></DropdownComponent>
-        <a href="https://canadapproval.com/blog/" className="button">
+        <a href="https://canadapproval.com/blog/" className="header-text">
           BLOG
         </a>
-        <button className="button">GET APPROVED</button>
+        <button className="header-button">GET APPROVED</button>
       </header>
       <main>MAIN</main>
       <footer>FOOTER </footer>
--- src/app/DropdownComponent/DropdownComponent.tsx
+++ src/app/DropdownComponent/DropdownComponent.tsx
@@ -4,15 +4,15 @@ import "./DropdownComponent.css";
 const DropdownComponent = () => {
   return (
     <div className="dropdown">
-      <a className="dropbtn">CAR LOANS</a>
+      <a className="dropdown-button">CAR LOANS</a>
       <div className="dropdown-content">
-        <a className="dropdown-btn" href="#">
+        <a className="dropdown-button" href="#">
           Link 1
         </a>
-        <a className="dropdown-btn" href="#">
+        <a className="dropdown-button" href="#">
           Link 2
         </a>
-        <a className="dropdown-btn" href="#">
+        <a className="dropdown-button" href="#">
           Link 3
         </a>
       </div>
